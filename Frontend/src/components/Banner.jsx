9c1f560722b8ff3882bdc0b8b91a3545e3a5c4c0
Srushti Borvadkar <srushtiborvadkar@gmail.com>
2025-01-09 15:10:52 +0530

    Backend Done!!

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -17,7 +17,7 @@ function Banner() {
   <input type="text" className="grow" placeholder="Email" />
 </label>
     </div>
-    <button className="btn mt-6 btn-secondary">Secondary</button>
+    <button className="btn mt-6 btn-secondary">Get Started</button>
       </div>
       <div className="w-full order-2 md:order-1 md:w-1/2">
         <img src={banner} className="w-92 h-92" alt="" />
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import Login from './Login'
 import { useForm } from "react-hook-form"
+import axios from "axios";
+import toast from 'react-hot-toast';
 
 function Signup() {
+  const location=useLocation()
+  const navigate=useNavigate()
+  const from=location.state?.from?.pathname || "/"
   const {
       register,
       handleSubmit,
       formState: { errors },
     } = useForm()
   
-    const onSubmit = (data) => console.log(data);
+    const onSubmit = async(data) => {
+      const userInfo={
+        fullname:data.fullname,
+        email:data.email,
+        password:data.password,
+      }
+      axios.post("http://localhost:4001/user/signup",userInfo)
+      .then((res)=>{
+        console.log(res.data)
+        if(res.data){
+          toast.success('Signup Successfully');
+          navigate(from,{replace:true});
+        }
+        localStorage.setItem("Users",JSON.stringify(res.data))
+      })
+      .catch((err)=>{
+        console.log(err)
+        toast.error("Error: "+err.response.data.message);
+      });
+    };
   return ( 
     <>
       <div className='flex h-screen items-center justify-center shadow-md'>
@@ -24,9 +48,9 @@ function Signup() {
             <div className='mt-4 space-y-2'>
                 <span>Name</span>
                 <br />
-                <input type="text" placeholder='Enter your full name'className='w-80 px-3 py-1 border rounded-md outline-none' {...register("name", { required: true })}/>
+                <input type="text" placeholder='Enter your full name'className='w-80 px-3 py-1 border rounded-md outline-none' {...register("fullname", { required: true })}/>
                 <br />
-        {errors.name && <span className='text-sm text-red-500'>This field is required</span>}
+        {errors.fullname && <span className='text-sm text-red-500'>This field is required</span>}
             </div>
             {/*Email*/}
             <div className='mt-4 space-y-2'>
@@ -40,7 +64,7 @@ function Signup() {
             <div className='mt-4 space-y-2'>
                 <span>Password</span>
                 <br />
-                <input type="password" placeholder='Enter your password'className='w-80 px-3 py-1 border rounded-md outline-none' {...register("password", { required: true })}/>
+                <input type="password" placeholder='Enter your password'className='w-80 px-3 py-1 border rounded-md outline-none text-black' {...register("password", { required: true })}/>
                 <br />
         {errors.password && <span className='text-sm text-red-500'>This field is required</span>}
             </div>
